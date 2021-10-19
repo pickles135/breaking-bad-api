1d6198d73a7e96086b9ad9ff88b6a4e158348c1a
tcho <teresacho135@gmail.com>
2021-10-19 11:57:09 -0700

    creates API call using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Header from './components/ui/Header';
+import axios from 'axios'
 
 const App = () => {
+      const [items, setItems] = useState([])
+      const [isLoading, setisLoading] = useState(true)
+
+      useEffect(() => {
+        const fetchItems = async () => {
+          const result = await axios(
+            `https://www.breakingbadapi.com/api/characters`
+          )
+
+          console.log(result.data)
+          setItems(result.data)
+          setisLoading(false)
+        }
+
+        fetchItems()
+      }, [])
+
   return (
     <div className="container">
-      Hello
+      <Header />
     </div>
   );
 }
